868c521648ab78fcac4183a67e2c4ac2b28e9353
Kostya Sigarov <ksigaryov@gmail.com>
2022-05-11 23:50:40 +0300

    Add functions: add new song, edit song

--- song.rb
+++ song.rb
@@ -10,6 +10,7 @@ class Song
   property :lyrics, Text
   property :length, Integer
   property :released_on, Date
+
 end
 
 DataMapper.finalize
@@ -19,7 +20,28 @@ get '/songs' do
   erb :songs
 end
 
-get '/song/:id' do
+get '/songs/new' do
+  @song = Song.new
+  erb :new_song
+end
+
+get '/songs/:id' do
   @song = Song.get(params[:id])
   erb :show_song
+end
+
+get '/songs/:id/edit' do
+  @song = Song.get(params[:id])
+  erb :edit_song
+end
+
+post '/songs' do
+  song = Song.create(params[:song])
+  redirect to("/songs/#{song.id}")
+end
+
+put '/songs/:id' do
+  song = Song.get(params[:id])
+  song.update(params[:song])
+  redirect to("/songs/#{song.id}")
 end
